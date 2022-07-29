59eaf44e12d09f6a9b78afb1656a7ecbdcf1e87e
itthipon <itthipon.d@ku.th>
2022-07-29 10:12:50 +0700

    make responsive web

--- domains/nida.upbean.co.th/public_html/application/views/enroll/create.php
+++ domains/nida.upbean.co.th/public_html/application/views/enroll/create.php
@@ -7,45 +7,43 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Enroll Subject</title>
     <style>
-        .mx-5{
-            margin-left: 37vw !important;
-            margin-right: 37vw !important;
-            margin-top: 4vw !important;
-        }
-        .button-center{
-            margin-left: 11vw;
-            margin-right: 11vw;
+        label h5 {
+            font-size: calc(60% + 0.8vmin);
         }
     </style>
 </head>
 <body>
-    <div class="card border-0 mx-5" style="width: 25rem;">
-        <h4 class="text-center my-3">ลงทะเบียนหลักสูตร</h4>
-        <form id="modal_form">
-            <div class="mb-3">
-                <label for="firstname" class="form-label">ชื่อจริง</label>
-                <input id="firstname" name="firstname" type="text" class="form-control" required>
-            </div>
-            <div class="mb-3">
-                <label for="lastname" class="form-label">นามสกุล</label>
-                <input id="lastname" name="lastname" type="text" class="form-control" required>
-            </div>
-            <div class="mb-3">
-                <label for="id" class="form-label">เลขบัตรประจำตัวประชาชน</label>
-                <input id="id" name="id" type="text" class="form-control" required>
-            </div>
-            <div class="mb-3">
-                <label for="tel" class="form-label">เบอร์มือถือ</label>
-                <input id="tel" name="tel" type="text" class="form-control" required>
-            </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-10 offset-1 col-sm-4 offset-sm-4">
+                <div class="card border-0">
+                    <h5 class="text-center my-3">ลงทะเบียนหลักสูตร</h4>
+                    <form id="modal_form">
+                        <div class="mb-3">
+                            <label for="firstname" class="form-label">ชื่อจริง</label>
+                            <input id="firstname" name="firstname" type="text" class="form-control" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="lastname" class="form-label">นามสกุล</label>
+                            <input id="lastname" name="lastname" type="text" class="form-control" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="id" class="form-label">เลขบัตรประจำตัวประชาชน</label>
+                            <input id="id" name="id" type="text" class="form-control" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="tel" class="form-label">เบอร์มือถือ</label>
+                            <input id="tel" name="tel" type="text" class="form-control" required>
+                        </div>
 
-            <input id="enroll_id" name="enroll_id" type="hidden">
-
-            <button  id="save_btn" class="btn btn-primary button-center" style="center" >Submit</button>
-            <!-- <button  onclick="FormatID('1104300278064')" class="btn btn-primary button-center" style="center" >Submit</button> -->
-        </form>
+                        <input id="enroll_id" name="enroll_id" type="hidden">
+                    </form>
+                    <button  id="save_btn" class="btn btn-primary">ลงทะเบียน</button>
+                </div>
+            </div>
+        </div>
     </div>
-    
+     
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
--- domains/nida.upbean.co.th/public_html/application/views/template/navbar.php
+++ domains/nida.upbean.co.th/public_html/application/views/template/navbar.php
@@ -7,24 +7,25 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title></title>
     <style>
-        .margin-nav{
-            margin-top: -1vw;
-            margin-bottom: -0.5vw;
-            margin-left: 3vw;
+        img {
+            width: 26rem;
+            height: 5rem;
         }
-        a:visited {
-            color: 	#008000;
+        a {
+            font-size: calc(60% + 0.8vmin);
+            /* text-decoration: none; */
         }
     </style>
 </head>
 <body>
-    <nav class="navbar navbar-light border-bottom shadow-sm rounded">
-        <img class="margin-nav" src="<?php echo "/assets/images/nida-navbar.png" ?>">            
+    <nav class="navbar navbar-light border-bottom shadow-sm rounded">                
+        <img src="<?php echo "/assets/images/nida-navbar.png" ?>"> 
         <div class="d-flex flex-row-reverse">
-            <a class='px-5'>ตรวจสอบการชำระเงิน</a>
-            <a href="<?php echo base_url(); ?>subject/enroll_subject" class='px-5'>ลงทะเบียนหลักสูตร</a>                       
+            <a href="" class='px-3' style="text-decoration: none; color: black;">ตรวจสอบการชำระเงิน</a>
+            <!-- <a href="<?php echo base_url(); ?>subject/enroll_subject" class='px-3'>ลงทะเบียนหลักสูตร</a>                        -->
         </div>
     </nav>
+    
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script>     
